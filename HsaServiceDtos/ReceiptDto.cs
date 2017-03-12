0c63e54b967ee4b57ed13cd499aae624d9f744d5
Pearse Hutson <pearse.2008@gmail.com>
2017-03-12 14:29:11 -0500

    Added DTOs from API

---
+++ HsaServiceDtos/ReceiptDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using HsaDotnetBackend.Helpers;
+
+namespace HsaServiceDtos
+{
+    public class ReceiptDto
+    {
+        public ReceiptDto()
+        {
+            this.LineItems = new List<LineItemDto>();
+        }
+        public int ReceiptId { get; set; }
+        public StoreDto Store { get; set; }
+        public DateTime? DateTime { get; set; }
+        public bool? IsScanned { get; set; }
+        public string ImageId { get; set; }
+        public string ImageUrl => ReceiptPictureHelper.GetReceiptImageUrl(ImageId);
+        public ICollection<LineItemDto> LineItems { get; set; }
+    }
+}
